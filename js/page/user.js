c30a5c76e963b5edcc3ecf32a59ebea13d2553dc
wangyufei3360 <wangyufei3360@126.com>
2015-06-15 10:07:14 +0800

    新增内容

--- js/page/user.js
+++ js/page/user.js
@@ -10,11 +10,36 @@ define(function(require, exports, module){
 	//dom页面结构加载完执行
 	$(document).ready(function(){
 
-		//用户中心左侧展开收缩效果
+		//用户中心左侧导航展开收缩效果
 		var sideItem = $(".m-side").find(".item").children("a");
 		$(sideItem).click(function () {
 			$(this).siblings("ul").slideToggle("slow");
 		})
+
+		//用户中心充值选择效果
+		var bankRadio = $(".m-bankOption").children("li").children("span");
+		var bankLi = $(".m-bankOption").children("li");
+
+		$(bankLi).click(function () {
+			$(bankLi).removeClass("current");
+			$(bankRadio).removeClass("u-radio-checked");
+			$(bankRadio).find("radio").prop("checked",false);
+
+			$(this).find("span").addClass("u-radio-checked");
+			$(this).addClass("current");
+			$(this).find("radio").prop("checked",true);
+		})
+
+		$(bankRadio).click(function () {
+			$(bankLi).removeClass("current");
+			$(bankRadio).removeClass("u-radio-checked");
+			$(bankRadio).find("radio").prop("checked",false);
+
+			$(this).addClass("u-radio-checked");
+			$(this).parent("li").addClass("current");
+			$(this).find("radio").prop("checked",true);
+		})
+
 	})
 
 })
