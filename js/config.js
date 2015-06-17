9664f76741ee6a911d14c10c077c2f2686854c06
wangyufei3360 <wangyufei3360@126.com>
2015-06-17 17:40:03 +0800

    新增内容

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ jpath.handlebars = typeof(debug) != 'undefined' && debug ? 'lib/handlebars/3.0.3
 jpath.mousewheel = typeof(debug) != 'undefined' && debug ? 'lib/mousewheel/3.1.11/jquery.mousewheel.js' : 'lib/mousewheel/3.1.11/jquery.mousewheel.min.js';
 jpath.validate = typeof(debug) != 'undefined' && debug ? 'lib/validation/1.11.1/jquery.validate.js' : 'lib/validation/1.11.1/jquery.validate.min.js';
 jpath.kinmaxshow = typeof(debug) != 'undefined' && debug ? 'lib/kinmaxshow/1.1/jquery.kinMaxShow-1.1.src.js' : 'lib/kinmaxshow/1.1/jquery.kinMaxShow-1.1.src.js';
-jpath.dialog = typeof(debug) != 'undefined' && debug ? 'lib/artDialog/6.0.4/dialog.js' : 'lib/artDialog/6.0.4/dialog-min.js';
+jpath.dialog = typeof(debug) != 'undefined' && debug ? 'lib/artDialog/6.0.4/dialog-plus.js' : 'lib/artDialog/6.0.4/dialog-plus-min.js';
 jpath.tabs = typeof(debug) != 'ubdefined' && debug ? 'lib/tabs/1.0/jquery.tabs.js' : 'lib/tabs/1.0/jquery.tabs.js';
 jpath.fancybox = typeof(debug) != 'ubdefined' && debug ? 'lib/fancybox/1.3.4/jquery.fancybox-1.3.4.js' : 'lib/fancybox/1.3.4/jquery.fancybox-1.3.4.js';
 jpath.jqueryslide = typeof(debug) != 'ubdefined' && debug ? 'lib/jqueryslide/1.0/jq.Slide.js': 'lib/jqueryslide/1.0/jq.Slide.js';
--- js/page/user.js
+++ js/page/user.js
@@ -6,6 +6,7 @@ define(function(require, exports, module){
 	//引入依赖文件
 	require('jquery');
 	require('validate');
+	require('dialog');
 
 	//dom页面结构加载完执行
 	$(document).ready(function(){
@@ -14,7 +15,7 @@ define(function(require, exports, module){
 		var sideItem = $(".m-side").find(".item").children("a");
 		$(sideItem).click(function () {
 			$(this).siblings("ul").slideToggle("slow");
-		})
+		});
 
 		//用户中心充值选择效果
 		var bankRadio = $(".m-bankOption").children("li").children("span");
@@ -29,12 +30,13 @@ define(function(require, exports, module){
 			$(this).addClass("current");
 			$(this).find("radio").prop("checked",true);
 
-			if( rechargeType )
-			//下方银行限额显示
-			var bankName = $(this).attr("data-name");
-			$(".m-bankNorm").find(".bankInfo").hide();
-			$("#chongzhi-" + bankName).show();
-		})
+			if( rechargeType == 1){
+				//下方银行限额显示
+				var bankName = $(this).attr("data-name");
+				$(".m-bankNorm").find(".bankInfo").hide();
+				$("#chongzhi-" + bankName).show();
+			}
+		});
 		$(bankRadio).click(function () {
 			$(bankLi).removeClass("current");
 			$(bankRadio).removeClass("u-radio-checked");
@@ -44,11 +46,13 @@ define(function(require, exports, module){
 			$(this).parent("li").addClass("current");
 			$(this).find("radio").prop("checked",true);
 
-			//下方银行限额显示
-			var bankName = $(this).parent("li").attr("data-name");
-			$(".m-bankNorm").find(".bankInfo").hide();
-			$("#chongzhi-" + bankName).show();
-		})
+			if(rechargeType == 1){
+				//下方银行限额显示
+				var bankName = $(this).parent("li").attr("data-name");
+				$(".m-bankNorm").find(".bankInfo").hide();
+				$("#chongzhi-" + bankName).show();
+			}
+		});
 
 		//个人充值表单验证
 		$("#pRechargeForm").validate({
@@ -67,7 +71,7 @@ define(function(require, exports, module){
 			errorPlacement: function (error, element) {
 				error.appendTo(element.parent().find(".ipt-err"));
 			}
-		})
+		});
 
 		//企业充值表单验证
 		$("#cRechargeForm").validate({
@@ -86,12 +90,156 @@ define(function(require, exports, module){
 			errorPlacement: function (error, element) {
 				error.appendTo(element.parent().find(".ipt-err"));
 			}
+		});
+
+		//基本信息 - 绑定手机表单验证-第一步
+		var phoneForm1 = $("#phoneEditForm1").validate({
+			rules:{
+				phonecode:{
+					required:true
+				},
+				password:{
+					required:true
+				}
+			},
+			messages:{
+				phonecode:{
+					required:"手机验证码不能为空"
+				},
+				password:{
+					required:"登录密码不能为空"
+				}
+			},
+			errorPlacement: function (error, element) {
+				error.appendTo(element.parent());
+			}
+		});
+
+		//基本信息 - 绑定手机表单验证-第二步
+		var phoneForm2 = $("#phoneEditForm2").validate({
+			rules:{
+				newphone:{
+					required:true,
+					isMobile:true
+				},
+				phonecode:{
+					required:true
+				}
+			},
+			messages:{
+				newphone:{
+					required:"手机号码不能为空",
+					isMobile:"手机号码格式不正确，请重新输入"
+				},
+				phonecode:{
+					required:"手机验证码不能为空"
+				}
+			},
+			errorPlacement: function (error, element) {
+				error.appendTo(element.parent());
+			}
+		});
+
+		// 点击修改弹出框
+		$("#j-phoneEdit").click(function () {
+			var d = dialog({
+				title: '绑定手机修改',
+				url:"userInfo/resetMobile.html",
+				height:320
+			});
+			d.showModal();
+		});
+
+		// 修改绑定手机第一步
+		$("#subbt-step1").click(function () {
+			//判断表单是否通过验证
+			if(phoneForm1.form()){
+				$("#phoneEditForm1").hide();
+				$("#phoneEditForm2").show()
+			}
+		});
+
+		// 修改绑定手机第二步
+		$("#subbt-step2").click(function () {
+			//判断表单是否通过验证
+			if(phoneForm2.form()){
+				$("#phoneEditForm2").hide();
+				$("#phoneEditForm3").show();
+			}
+		});
+
+		//邮箱绑定验证
+		var emailForm = $("#emailEditForm").validate({
+			rules:{
+				email:{
+					required:true,
+					email:true
+				}
+			},
+			messages:{
+				email:{
+					required:"邮箱不能为空",
+					email:"邮箱格式不正确，请重新输入"
+				}
+			},
+			errorPlacement: function (error, element) {
+				error.appendTo(element.parent());
+			}
+		});
+
+		//基本信息 - 邮箱验证
+		$("#j-emailEdit").click(function () {
+			var d = dialog({
+				title: '邮箱验证',
+				url:"userInfo/bindEmail.html",
+				height:300
+			});
+			d.showModal();
+		})
+
+		//修改密码 - 表单验证
+		var pwdForm = $("#pwdEditForm").validate({
+			rules:{
+				oldPlainPassword:{
+					required:true,
+					rangelength:[6,20]
+				},
+				newPlainPassword:{
+					required:true,
+					rangelength:[6,20]
+				},
+				renewPlainPassword:{
+					required:true,
+					equalTo:"#newPlainPassword"
+				},
+			},
+			messages:{
+				oldPlainPassword:{
+					required:"用户名不能为空",
+					rangelength:"请输入6-16位字符，可以是字母、数字、中文"
+				},
+				newPlainPassword:{
+					required:"密码不能为空",
+					rangelength:"密码长度只能在6-20位字符之间"
+				},
+				renewPlainPassword:{
+					required:"确认密码不能为空",
+					equalTo:"您输入的密码不一致"
+				}
+			},
+			errorPlacement: function (error, element) {
+				error.appendTo(element.parent());
+			}
+		});
+
+		//基本信息 - 修改密码
+		$("#j-passwordEdit").click(function () {
+			var d = dialog({
+				title: '邮箱验证',
+				url:"userInfo/updatePwd.html",
+				height:350
+			});
+			d.showModal();
 		})
 	})
-
-	function bankOpt(type){
-
-	}
-
-
 })
